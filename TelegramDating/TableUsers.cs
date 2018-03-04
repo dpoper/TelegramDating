118abe844c3a0a230d49cd847e12e053bb4957a3
Mishka1337 <mixakos98@yandex.ru>
2018-03-04 23:47:03 +0400

    db

---
+++ TelegramDating/TableUsers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramDating
+{
+    [Table(Name = "Users")]
+    public static class TableUsers
+    {
+
+    }
+
+    [Database]
+    public static class DB { }
+}
